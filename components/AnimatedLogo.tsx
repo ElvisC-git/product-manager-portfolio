f3fc19aa914dd4f36e9a8c5dbe4a200fe681ae9b
ElvisC-git <112125193+ElvisC-git@users.noreply.github.com>
2025-08-25 00:15:01 +0200

    Update AnimatedLogo.tsx

--- components/AnimatedLogo.tsx
+++ components/AnimatedLogo.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export const AnimatedLogo = () => {
   return (
     <div className="relative h-64 w-64 sm:h-80 sm:w-80 md:h-[28rem] md:w-[28rem] lg:h-[32rem] flex items-center justify-center animate-fade-in-delayed">
-      <img 
-        src="/wai_gif.png" 
+      <img src="/product-manager-portfolio/wai_gif.png" 
         alt="Wai´s animated GIF" 
         className="w-full h-full object-contain"
       />
