2260927ff468a72632d983098aabe04094e6501b
Wai Cheung <waicheung@Elviss-MBP.fritz.box>
2025-08-24 21:36:00 +0200

    Install React dependencies and fix Vite setup

--- components/AnimatedLogo.tsx
+++ components/AnimatedLogo.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const AnimatedLogo = () => {
   return (
-    <div className="relative h-64 w-64 sm:h-80 sm:w-80 md:h-[28rem] md:w-[28rem] lg:h-[32rem] lg:w-[32rem]">
+    <div className="relative h-64 w-64 sm:h-80 sm:w-80 md:h-[28rem] md:w-[28rem] lg:h-[32rem] flex items-center justify-center animate-fade-in-delayed">
       <img 
-        src="/assets/logo.png" 
-        alt="Wai Cheung Logo" 
-        className="h-full w-full object-contain"
+        src="./assets/logo.gif" 
+        alt="Personal animated logo" 
+        className="w-full h-full object-contain"
       />
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/product-manager-portfolio/',
+  build: {
+    outDir: 'docs',
+  },
 })
